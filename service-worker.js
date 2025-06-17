8ad2df15d0b22fcfc4ded558d91f67bbc41e78d2
Dheeraj Sena <125935208+dheerajsena@users.noreply.github.com>
2025-06-17 14:21:49 +1000

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,23 @@
+const CACHE = 'tradeflow-v2';
 self.addEventListener('install', e => {
+  self.skipWaiting();
   e.waitUntil(
-    caches.open('tradeflow-v1').then(cache => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './manifest.json',
-        './icon-192.png',
-        './icon-512.png'
-      ]);
-    })
+    caches.open(CACHE).then(cache => cache.addAll([
+      './',
+      './index.html',
+      './manifest.json',
+      './icon-192.png',
+      './icon-512.png'
+    ]))
   );
 });
-
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))
+    ).then(() => self.clients.claim())
+  );
+});
 self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(response => response || fetch(e.request))
